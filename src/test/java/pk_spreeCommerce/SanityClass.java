666468307d7e7e74e94c65d8c66b5b8e2a0b83fa
nive-dhini <nivedhiniramesh05@gmail.com>
2024-03-20 17:05:33 +0530

    Modified POM file

--- src/test/java/pk_spreeCommerce/SanityClass.java
+++
@@ -1,9 +0,0 @@
-package pk_spreeCommerce;
-
-import org.testng.annotations.Test;
-
-public class SanityClass {
-  @Test
-  public void f() {
-  }
-}
---
+++ src/test/java/pk_spreeCommerce/SanityTest.java
@@ -0,0 +1,14 @@
+package pk_spreeCommerce;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+
+public class SanityTest {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+}
